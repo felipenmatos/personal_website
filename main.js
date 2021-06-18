90e8d5a1036601ea9dd1dbad687d850d15a9c3f2
Felipe Nonato <82072640+Lipe-27@users.noreply.github.com>
2021-06-18 10:34:50 -0300

    atualizacao js

--- main.js
+++ main.js
@@ -26,4 +26,36 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+// Scroll suave com links internos
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
 
+function scrollToSection(event) {
+  event.preventDefault()
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  const topo = section.offsetTop;
+  
+  window.scrollTo({
+    top: topo,
+    behavior: 'smooth'
+  })
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection); 
+});
+
+//Animacao scroll 
+
+const sections = document.querySelectorAll('.js-scroll');
+
+function animaScroll() {
+  sections.forEach((section)=>{
+    const sectionTop = section.getBoundingClientRect().top;
+    if(sectionTop < 0) {
+      section.classList.add('ativo');
+    }
+  })
+}
+
+window.addEventListener('scroll', animaScroll);
